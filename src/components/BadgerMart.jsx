b67f8a35ec9dd611b7238038de5e2822ad107047
Clement Yanze Lu <lucaslyz03@gmail.com>
2024-07-29 00:16:20 +0800

    hw3 finished

--- src/components/BadgerMart.jsx
+++ src/components/BadgerMart.jsx
@@ -1,15 +1,18 @@
 import { useEffect, useState } from "react"
+import "./BadgerMart.css"
 import BadgerSaleItem from "./BadgerSaleItem";
 import { Col, Container, Row } from "react-bootstrap";
+
 
 export default function BadgerMart(props) {
 
     const [saleItems, setSaleItems] = useState([]);
+    const [firstline, setFirstline] = useState([]);
 
     useEffect(() => {
         fetch("https://cs571.org/api/s24/hw3/all-sale-items", {
             headers: {
-                "X-CS571-ID": CS571.getBadgerId()
+                "X-CS571-ID": "bid_fa_dee295439886d4d7515fc8ecd9f525126add11e5a00bf859fc78b2ff44bd66a8" 
             }
         })
         .then(res => res.json())
@@ -19,9 +22,23 @@ export default function BadgerMart(props) {
         })
     }, [])
 
+
+    useEffect(()=>{
+        fetch("https://cs571.org/api/s24/hw3/featured-sale-item", {
+            headers: {
+                "X-CS571-ID": "bid_fa_dee295439886d4d7515fc8ecd9f525126add11e5a00bf859fc78b2ff44bd66a8" 
+            }
+        })
+        .then(res => res.json())
+        .then(data => {
+            setFirstline(data);
+        })
+    },[])
+
     return <div>
         <h1>Badger Mart</h1>
-        <p>Welcome to our small-town mini mart located in Madison, WI!</p>
+        <h3>Welcome to our small-town mini mart located in Madison, WI!</h3>
+        <h5>Today's featured item is {firstline.name} for ${firstline.price} !</h5>
         <Container>
             <Row>
             {
@@ -32,6 +49,7 @@ export default function BadgerMart(props) {
                             description={saleItem.description}
                             price={saleItem.price}
                             featured={saleItem.featured}
+                            
                         />
                     </Col>
                 })
--- src/components/BadgerSaleItem.jsx
+++ src/components/BadgerSaleItem.jsx
@@ -1,10 +1,27 @@
+import { useState } from "react"
+import './BadgerSaleItem.css'
+
+
 export default function BadgerSaleItem(props) {
-    return <div>
-        <h2>{props.name}</h2>
-        <div>
-            <button className="inline">-</button>
-            <p className="inline">0</p>
-            <button className="inline">+</button>
-        </div>
+    const [itemnum, setitemnum] = useState(0)
+
+    let decreaseq = () =>{
+        setitemnum(itemnum => itemnum > 0 ? itemnum - 1 : itemnum)
+    }
+    
+    let increaseq = () =>{
+        setitemnum(itemnum => itemnum + 1)
+    }
+
+    
+    return <div className={props.featured ? "b_special" : "b_ordinary"}>
+            <h2>{props.name}</h2>
+            <p>{props.description}</p>
+            <p>{props.price}</p>
+            <div className="undertype">
+                <button className="inline" onClick={decreaseq} disabled={itemnum <= 0}>-</button>
+                <p className="inline">{itemnum}</p>
+                <button className="inline" onClick={increaseq}>+</button>
+            </div>
     </div>
 }
